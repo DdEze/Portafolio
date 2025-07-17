75c60f7f3a615ce6729ae31fc82285b596aec263
Ezequiel <ezequielensenada@yahoo.com.ar>
2025-07-17 19:52:53 -0300

    Navbar adaptado a movil

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 
 const Navbar = ({ darkMode, setDarkMode }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">De Dominicis Ezequiel</div>
-      <ul className="navbar-links">
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/about">Acerca de mí</Link></li>
-        <li><Link to="/projects">Proyectos</Link></li>
+
+      {/* Botón Hamburguesa */}
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Inicio</Link></li>
+        <li><Link to="/about" onClick={() => setMenuOpen(false)}>Acerca de mí</Link></li>
+        <li><Link to="/projects" onClick={() => setMenuOpen(false)}>Proyectos</Link></li>
         <li>
-          <a href="/C.V.pdf" download="De_Dominicis_CV.pdf" className="download-cv">
+          <a
+            href="/C.V.pdf"
+            download="De_Dominicis_CV.pdf"
+            className="download-cv"
+            onClick={() => setMenuOpen(false)}
+          >
             Descargar CV
           </a>
         </li>
         <li>
-          <button onClick={() => setDarkMode(!darkMode)} className="toggle-dark">
+          <button
+            onClick={() => {
+              setDarkMode(!darkMode);
+              setMenuOpen(false);
+            }}
+            className="toggle-dark"
+          >
             {darkMode ? "☀️" : "🌙"}
           </button>
         </li>
