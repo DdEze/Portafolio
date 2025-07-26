9f1a10e2c78d8114029252b0b0d008c6e91348d0
Ezequiel <ezequielensenada@yahoo.com.ar>
2025-07-25 22:12:39 -0300

    Se borror lo que estaba en el footer

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -35,19 +35,6 @@ const Contact = () => {
         <textarea name="message" placeholder="Tu mensaje" required></textarea>
         <button type="submit">Enviar</button>
       </form>
-
-      <div className="contact-info">
-        <h3>Información de contacto</h3>
-        <p>Email: ezequieldedominicis@gmail.com</p>
-        <div className="social-links">
-          <a href="https://www.linkedin.com/in/ezequiel-de-dominicis-435609205/" target="_blank" rel="noopener noreferrer">
-            <img src="/linkedin.png" alt="LinkedIn" className="social-icon" />
-          </a>
-          <a href="https://github.com/DdEze" target="_blank" rel="noopener noreferrer">
-            <img src="/github.webp" alt="GitHub" className="social-icon" />
-          </a>
-        </div>
-      </div>
     </section>
   );
 };
